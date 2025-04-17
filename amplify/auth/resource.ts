058e36b9d53e643ab5912b3bab984c7b4f74ab8b
Dennis Kung <denniskung68@hotmail.com>
2025-04-17 22:19:55 +1000

    test auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,18 +8,11 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
     externalProviders: {
-      google: {
-        clientId: secret('535189192813-nqu2lcrhhv8nd8hi53v7b146bhtu8qcs.apps.googleusercontent.com'),
-        clientSecret: secret('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'),
-        attributeMapping: {
-          email: 'email'
-        }
-      },
       callbackUrls: [
         'http://localhost:5173/profile',
         'https://www.d2ai.info/profile'
       ],
-      logoutUrls: ['http://localhost:3000/', 'https://www.d2ai.info']
+      logoutUrls: ['http://localhost:5173/', 'https://www.d2ai.info']
     }
   }
 });
