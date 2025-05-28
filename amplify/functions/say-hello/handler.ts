8edef09c49efd15302c0c084f4730d4e3608db8c
Dennis Kung <denniskung68@hotmail.com>
2025-05-28 14:29:05 +0900

    test lambda function 2

--- amplify/functions/say-hello/handler.ts
+++ amplify/functions/say-hello/handler.ts
@@ -3,6 +3,13 @@ import type { Schema } from "../../data/resource"
 export const handler: Schema["sayHello"]["functionHandler"] = async (event) => {
   // arguments typed from `.arguments()`
   const { name } = event.arguments
-  // return typed from `.returns()`
+  // return typed from `.returns()
+  console.log(JSON.stringify({
+    level: 'INFO',
+    message: 'sayHello function invoked',
+    name,
+    timestamp: new Date().toISOString()
+  }))
+
   return `Hello, ${name}!`
 }
